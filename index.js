50b604434822c46d62249e5fe3b9f8fa6032805e
Matthew Luc Sterling <mlucsterling@gmail.com>
2020-07-19 16:03:17 -0700

    initial commit, add JS function stubs

---
+++ index.js
@@ -0,0 +1,50 @@
+/*take info from date*/
+
+
+/*take info from venue name*/
+
+
+/*take info from venue address*/
+
+
+/*fetch from SunriseSunset API*/
+
+
+/*fetch from Openweather API*/
+
+
+/*fetch from Google Image Search API*/
+
+
+/*reveal results*/
+
+
+/*display sunrise*/
+
+
+/*display sunset*/
+
+
+/*display weather*/
+
+
+/*display Google Images*/
+
+
+/*create Streetview URL*/
+
+
+/*pass Google Streetview URL*/
+
+
+/*redirect to Google Streetview on button click*/
+
+
+/*handle click submit button*/
+function clickSubmit() {
+    $('form').submit(function() {
+        
+    })
+}
+
+/*handle app*/
